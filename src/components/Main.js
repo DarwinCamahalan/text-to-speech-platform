7628f2484f8ddb35a638540add0bbb24383d7928
Darwin <camahalandarwin@gmail.com>
2020-11-14 15:01:32 +0800

    Alpha 0.01

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,7 @@ import { useSpeechSynthesis, useSpeechRecognition } from "react-speech-kit";
 
 import "../styles/style.css";
 
-import {
-  Container,
-  Card,
-  CardBody,
-  FormGroup,
-  Input,
-  Label,
-  Button,
-} from "reactstrap";
+import { Card, CardBody, FormGroup, Input, Label, Button } from "reactstrap";
 
 export default function Main() {
   const [value, setValue] = React.useState("");
@@ -23,22 +15,17 @@ export default function Main() {
   });
   return (
     <>
-      <header className="container mx-auto ">
-        <Container fluid className="mobile-view container mx-auto pt-5">
+      <header className="container mx-auto pt-3">
+        <div className="input-bg">
           <Card className="card ">
             <FormGroup>
               <Label for="textInput">
-                <h5 className="py-3">
-                  Input Text Here or Click the{" "}
-                  <i className="micicon fas fa-microphone px-1"></i> icon.
-                  <div className="micicon2 m-0 p-0">
-                    <img
-                      src="/assets/video/gif1.gif"
-                      alt="gif"
-                      width="120px"
-                      height="40px"
-                    />
-                  </div>
+                <h5 className="py-3 text-white">
+                  <strong>
+                    {" "}
+                    Input Text Here or Click the{" "}
+                    <i className="micicon fas fa-microphone px-1"></i> icon.
+                  </strong>
                 </h5>
               </Label>
               <Input
@@ -50,10 +37,13 @@ export default function Main() {
               />
             </FormGroup>
             {listening && (
-              <h6>
-                Speak now, Im listening{" "}
-                <i className="text-danger fas fa-volume-up"></i>
-              </h6>
+              <h5 className="text-white">
+                <strong>
+                  {" "}
+                  Speak now, I'm listening Human.{" "}
+                  <i className="text-danger fas fa-volume-up"></i>
+                </strong>
+              </h5>
             )}
             <CardBody className="mx-auto text-center m-0 p-0">
               <Button
@@ -72,15 +62,15 @@ export default function Main() {
               </Button>
             </CardBody>
           </Card>
-        </Container>
+        </div>
         <img
-          className="robot"
+          className="robot3"
           src="/assets/video/gif4.gif"
           alt="gif"
           width="100%"
         />
         <img
-          className="robot"
+          className="robot3"
           src="/assets/video/gif2.gif"
           alt="gif"
           width="100%"
