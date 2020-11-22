5e6dbae071b2450e022285047848935806b23ac9
Darwin <camahalandarwin@gmail.com>
2020-11-22 16:22:05 +0800

    Added Voices

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -15,7 +15,7 @@ export default function MusicPlayer() {
           <AudioPlayer
             className="music"
             autoPlay
-            volume="0.2"
+            volume="0.4"
             loop={isLoopable}
             src="/assets/music/electric.mp3"
             onPlay={(e) => console.log("onPlay")}
